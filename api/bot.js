588364c51cce5da99fb18b79d9eec1fad48097cc
NattiSamson <45117900+NattiSamson@users.noreply.github.com>
2026-09-06 19:44:35 +0300

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -340,7 +340,7 @@ bot.on("message:text", async (ctx, next) => {
       
       const receipt = result.receipt;
       await ctx.reply(
-      "successfull \n\n" + receipt.receiptNo + "\n" + receipt.payerName + "\n" + receipt.payerTelebirrNo + "\n" + receipt.creditedPartyName + "\n" + receipt.creditedPartyAccountNo + "\n" + new Date(receipt.paymentDate).toLocaleDateString() + "\n" + receipt.settledAmount.replace(/[^0-9.]/g, ""),        
+      "successfull \n\n" + receipt.receiptNo + "\n" + receipt.payerName + "\n" + receipt.payerTelebirrNo + "\n" + receipt.creditedPartyName + "\n" + receipt.creditedPartyAccountNo + "\n" + receipt.paymentDate.split(" ")[0] + "\n" + receipt.settledAmount.replace(/[^0-9.]/g, ""),        
     );
       // Deposit was successfully processed
     delete pendingDeposit[telegramId];
