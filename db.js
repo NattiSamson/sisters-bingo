51bda0f4d3cecb4a2f40482d4d557f85b3ad39f6
NattiSamson <45117900+NattiSamson@users.noreply.github.com>
2026-09-07 22:53:53 +0300

    Update db.js

--- db.js
+++ db.js
@@ -26,25 +26,25 @@ module.exports = {
   },
 
   async approveDepositttttttttttt(receipt) {
-	  await ctx.reply("pro \n\n");
+	  
 const u = await pool.query('SELECT count(id) FROM deposits WHERE reference=$1', receipt.receiptNo);
-		 await ctx.reply("already used receipt no \n\n");
+		 
 		if(u > 0)
 		{
-   await ctx.reply("already used receipt no \n\n");
-		  return false;
+  
+		  return 1;
 		}
 	u = await pool.query('SELECT count(id) FROM payment_accounts WHERE IsActive = TRUE && RIGHT(account_number,4) = RIGHT($1,4)', receipt.creditedPartyAccountNo);
 		if(u > 0)
 		{
-		  await ctx.reply("invalid account number! \n\n");
-   return false;
+		  
+   return 2;
 		}
 	u = await pool.query('SELECT count(id) FROM payment_accounts WHERE IsActive = TRUE && account_name=$1', receipt.creditedPartyName);
 		if(u > 0)
 		{
-		  await ctx.reply("invalid account name! \n\n");
-   return false;
+		 
+   return 3;
 		}
 		const amount = await pool.query('SELECT amount FROM users WHERE telegram_id=$1', ctx.from.id);
 		u = await pool.query('INSERT INTO deposits(user_id,payment_account_id,deposit_method_id,depositor_name,depositor_account,amount,amount_after,reference,created_at) VALUES($1,$2,$3,$4,$5,$6,$7,$8,NOW()) RETURNING id', [user_id,1,1,creditedPartyName,receipt.creditedPartyAccountNo,receipt.settledAmount.replace(/[^0-9.]/g, ""),amount + receipt.settledAmount.replace(/[^0-9.]/g, ""),receipt.receiptNo]);
