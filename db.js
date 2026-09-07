19fef31f9efd474df20ad44b86e058bd846719a3
NattiSamson <45117900+NattiSamson@users.noreply.github.com>
2026-09-07 23:47:17 +0300

    Fix insertion query to await database operation

--- db.js
+++ db.js
@@ -47,7 +47,7 @@ const  u2  = await pool.query('SELECT count(id) FROM payment_accounts WHERE is_a
    return 3;
 		}
 		const {rows} = await pool.query('SELECT balance FROM users WHERE telegram_id=$1', [id]);
-		const { u4 } = await pool.query('INSERT INTO deposits(user_id,payment_account_id,deposit_method_id,depositor_name,depositor_account,amount,amount_after,reference,created_at) VALUES($1,$2,$3,$4,$5,$6,$7,$8,NOW()) RETURNING id', [user_id,1,1,creditedPartyName,receipt.creditedPartyAccountNo,receipt.settledAmount.replace(/[^0-9.]/g, ""),rows.rows[0].amount + receipt.settledAmount.replace(/[^0-9.]/g, ""),receipt.receiptNo]);
+		await pool.query('INSERT INTO deposits(user_id,payment_account_id,deposit_method_id,depositor_name,depositor_account,amount,amount_after,reference,created_at) VALUES($1,$2,$3,$4,$5,$6,$7,$8,NOW()) RETURNING id', [user_id,1,1,creditedPartyName,receipt.creditedPartyAccountNo,receipt.settledAmount.replace(/[^0-9.]/g, ""),rows.rows[0].amount + receipt.settledAmount.replace(/[^0-9.]/g, ""),receipt.receiptNo]);
 		return 4;
       },
 
