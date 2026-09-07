e26c727a6bd6b8217f130a9e55489331229c1743
NattiSamson <45117900+NattiSamson@users.noreply.github.com>
2026-09-07 23:37:06 +0300

    Fix variable declaration for u2 in db.js

--- db.js
+++ db.js
@@ -34,7 +34,7 @@ const  u   = await pool.query('SELECT count(id) FROM deposits WHERE reference=$1
   
 		  return 1;
 		}
-const { u2 } = await pool.query('SELECT count(id) FROM payment_accounts WHERE is_active = TRUE AND RIGHT(account_number,4) = RIGHT($1,4)', [receipt.creditedPartyAccountNo]);
+const  u2  = await pool.query('SELECT count(id) FROM payment_accounts WHERE is_active = TRUE AND RIGHT(account_number,4) = RIGHT($1,4)', [receipt.creditedPartyAccountNo]);
 		if(Number(u2.rows[0].count) > 0)
 		{
 		  
