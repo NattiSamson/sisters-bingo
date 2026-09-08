16a20f74b623ca84cab7c22dedc53adba533d39d
NattiSamson <45117900+NattiSamson@users.noreply.github.com>
2026-09-08 10:55:37 +0300

    Update db.js

--- db.js
+++ db.js
@@ -83,8 +83,8 @@ await pool.query(
     receipt.receiptNo
   ]
 );
-	  await this.query("UPDATE users SET balance=$1 WHERE telegram_id=$2", [amountAfter,id]);
-     await this.query("UPDATE payment_accounts SET balance=balance+$1 WHERE id=$2", [depositAmount,u2[0].id]);
+	  await pool.query("UPDATE users SET balance=$1 WHERE telegram_id=$2", [amountAfter,id]);
+     await pool.query("UPDATE payment_accounts SET balance=balance+$1 WHERE id=$2", [depositAmount,u2[0].id]);
 	  
 	  return 4;
       },
