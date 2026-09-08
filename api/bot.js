a6df482636062eb969cd9195b0e9ed3a4f49c55e
NattiSamson <45117900+NattiSamson@users.noreply.github.com>
2026-09-08 12:48:21 +0300

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -273,25 +273,49 @@ async function showDeposit(ctx) {
       "Please /start to register first."
     );
   }
-  const accounts = await db.getPaymentMethods();
-  
-  const buttons = accounts.map(account => [
+  const mes = "";
+  const paymentmethods = await db.getPaymentMethods();
+  const paymentmethodtypes = getPaymentMethodTypes();
+  if(paymentmethodtypes.length === 0)
+  {
+	  mes = "ይቅርታ! ለጊዜው የክፍያ መንገድ አልተዘጋጀም::"
+  }
+  if(paymentmethodtypes.length === 0)
+  {
+	  mes = "ይቅርታ! ለጊዜው የክፍያ መንገድ አልተዘጋጀም::"
+  }
+mes = "❇️ ብር ማስገባት የሚችሉት ቀጥሎ በተቀመጡት የ";
+if(paymentmethods.length === 1)
+{	
+	mes = mes + paymentmethods[0].amharic_name;
+	mes = mes + " አማራጮች ብቻ ነው።\n\n";	
+}
+else
+{
+	for (let i = 0; i < paymentmethods.length-1; i++) 
+		{
+			mes = mes + paymentmethods[i].amharic_name ", ";
+		}
+	mes = mes + paymentmethods[paymentmethods.length-1].amharic_name;	
+	mes = mes + " አማራጮች ብቻ ነው።\n\n";
+	
+}
+mes = mes + "🚫 ከዚህ ዉጭ የላከ አናስተናግድም 🚫\n\n";
+  const buttons = paymentmethods.map(pm => [
   {
 
-    text: `${account.emoji} ${account.amharic_name}`,
-    callback_data: `payment_${account.id}`
+    text: `${pm.emoji} ${pm.amharic_name}`,
+    callback_data: `payment_${pm.id}`
   }
-]);	// Add cancel button at the end
+]);	
+	// Add cancel button at the end
 buttons.push([
   {
     text: "❌ ሰርዝ",
     callback_data: "canceldeposit"
   }
 ]);
-  await ctx.reply(
-    "❇️ ብር ማስገባት የሚችሉት አሁን በተቀመጠዉ የTelebirr አካዉንት ብቻ ነዉ።\n\n" +
-    "🚫 ከዚህ ዉጭ የላከ አናስተናግድም 🚫\n\n" +
-    "👇 Telebirr የሚለዉን ይምረጡ 👇",
+  await ctx.reply(mes,
     {
       parse_mode: "Markdown",
       reply_markup: {
