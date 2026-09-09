4e08cba77e815afcba197bfcb3834a903e6e2e60
NattiSamson <45117900+NattiSamson@users.noreply.github.com>
2026-09-09 10:25:00 +0300

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -98,7 +98,7 @@ bot.command("broadcast", async (ctx) => {
       failed++;
       console.log(
         `Failed to send to ${user.telegram_id}:`,
-        err.message
+        error.response?.description || error.message
       );
     }
   }
