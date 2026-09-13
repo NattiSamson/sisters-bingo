16c0b61c51b73a4960b45c4a44611a5ddf3f626b
NattiSamson <45117900+NattiSamson@users.noreply.github.com>
2026-09-13 20:17:24 +0300

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -3558,7 +3558,10 @@ if (!admin) {
         `⚪ Inactive Users: *${stats.inactiveUsers}*\n` +
         `🔴 Blocked Users: *${stats.blockedUsers}*\n\n` +
 
-        `👑 Administrators: *${stats.administrators}*`;
+        `👑 Main Admin: *${stats.main_admin}*`;
+        `📊 Statistics Admin: *${stats.statistics_admin}*`;
+        `💸 Withdrawal Admin: *${stats.withdrawal_admin}*`;
+        `📢 Broadcast Admin: *${stats.broadcast_admin}*`;
 
 
       await ctx.reply(
