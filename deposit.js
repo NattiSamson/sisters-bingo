abdcb84d7c4c796efa5704a5ce162e8c690d2780
NattiSamson <45117900+NattiSamson@users.noreply.github.com>
2026-09-18 16:54:11 +0300

    Update deposit.js

--- deposit.js
+++ deposit.js
@@ -221,8 +221,7 @@ async function extractTransactionInfo(url) {
 async function extractTransactionInfofromThirdParty(typeName, receiptId) 
 {
   try 
-  {
-      pmName == "telebirr";
+  {      
       const response = await fetch(`https://checkit.et/api/process.php?type=${encodeURIComponent(typeName.toLowerCase())}&receiptid=${encodeURIComponent(receiptId)}`);
       const data = await response.json();
       if (!response.ok || !data.ok) 
